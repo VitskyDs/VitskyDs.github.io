049d17f36ceb803cc24e98a01e2a9d4d999f54c2
Vitsky <issac@showbox.com>
2017-12-13 16:48:59 +0100

    Added portfolio

--- GsapTitles/js/script.js
+++ GsapTitles/js/script.js
@@ -4,14 +4,24 @@ const btnPrev = $('.video-controls-prev-button'),
     btnPlay = $('.video-controls-play-button'),
     btnControls = $('.video-controls'),
     btnAdd = $('.title-add'),
-    video = $('#video');
+    video = document.getElementById('video');
 
+const animUnits = [0.15, 0.2, 0.25, 0.5, 1];
 /*Sidebar*/
 
 
 
 
 /* Video Controls*/
+
+btnPlay.click(function () {
+    //video.paused ? video.play() : video.pause();
+    themeTl.paused ? themeTl.play() : themeTl.pause();
+});
+
+btnPrev.click(function () {
+    themeTl.progress(0);
+});
 
 /*Timeline*/
 /*Listen to change of Title*/
@@ -27,6 +37,8 @@ $('.timeline').on('click', '.title-wrapper', function () {
     // manipulate sidebar
     $('.title-options').addClass('display-none');
     $(currentTitle).removeClass('display-none');
+    // change video contents
+
 });
 
 /*Add a title*/
@@ -38,10 +50,22 @@ $('.title-add-item').click(function () {
     $('.title-options').addClass('display-none');
     // add content title-super-3 or title-list-2 and replace title-wrapper
     if ($(this).hasClass('title-super-3')) {
-        $('<div id="super-3-wrapper" class="title-options"><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-title"></div><div class="panel-heading">Titles</div></div><div class="sidebar-panel-content"><div class="sidebar-text-field"><div class="label small">First line</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-style"></div><div class="panel-heading">Styles</div></div><div class="sidebar-panel-content"><div class="sidebar-style-item"></div><div class="sidebar-style-item"></div><div class="sidebar-style-item"></div><div class="sidebar-style-item"></div></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-duration"></div><div class="panel-heading">Duration</div></div><div class="sidebar-panel-content"><input type="range" min="3" max="10" step="0.5"></div></div>').insertAfter('#super-2-wrapper');
+        $('<div id="super-3-wrapper" class="title-options"><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-title"></div><div class="panel-heading">Titles</div></div><div class="sidebar-panel-content"><div class="sidebar-text-field"><div class="label small">First line</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-style"></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-duration"></div><div class="panel-heading">Duration</div></div><div class="sidebar-panel-content"><input type="range" min="3" max="10" step="0.5"></div></div>').insertAfter('#super-2-wrapper');
         $('.title-add').replaceWith('<div class="title-wrapper title-super-3 title-wrapper-active"></div>');
     } else {
-        $('<div id="list-2-wrapper" class="title-options"><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-title"></div><div class="panel-heading">Titles</div></div><div class="sidebar-panel-content"><div class="sidebar-text-field"><div class="label small">First item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Second item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="Last name, Desctiption, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Third item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Fourth item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="Last name, Desctiption, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Fifth item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-style"></div><div class="panel-heading">Styles</div></div><div class="sidebar-panel-content"><div class="sidebar-style-item"></div><div class="sidebar-style-item"></div><div class="sidebar-style-item"></div><div class="sidebar-style-item"></div></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-duration"></div><div class="panel-heading">Duration</div></div><div class="sidebar-panel-content"><input type="range" min="3" max="10" step="0.5"></div></div>').insertAfter('#super-2-wrapper');
+        $('<div id="list-2-wrapper" class="title-options"><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-title"></div><div class="panel-heading">Titles</div></div><div class="sidebar-panel-content"><div class="sidebar-text-field"><div class="label small">First item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Second item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="Last name, Desctiption, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Third item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Fourth item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="Last name, Desctiption, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div><div class="sidebar-text-field"><div class="label small">Fifth item</div><div class="input-wrapper"><input type="text" maxlength="25" placeholder="First name, Headline, etc." class="text-input text-input-super"><div class="input-char-counter">25</div></div></div></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-style"></div><div class="sidebar-panel-heading"><div class="panel-icon panel-icon-duration"></div><div class="panel-heading">Duration</div></div><div class="sidebar-panel-content"><input type="range" min="3" max="10" step="0.5"></div></div>').insertAfter('#super-2-wrapper');
         $('.title-add').replaceWith('<div class="title-wrapper title-list-2 title-wrapper-active"></div>');
     }
 });
+
+
+/*Animations*/
+const themeTl = new TimelineMax({
+    paused: true
+});
+
+themeTl.add("start", 0).from('.opener-text-1', animUnits[2], {
+    y: 300
+}, "start").from('.opener-text-2', animUnits[2], {
+    y: -300
+}, "start");
---
+++ portfolio/js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+
+    const mainContent = $('main'),
+        header = $('.header'),
+        menuButton = $('.menu-button'),
+        mobileMenu = $('.mobile-menu');
+
+
+
+    /*Initialize Flickity Carousels*/
+
+    $('.main-carousel').flickity({
+        // options
+        cellAlign: 'center',
+        contain: true,
+        imagesLoaded: true,
+        setGallerySize: true,
+        prevNextButtons: false,
+        pageDots: false
+    });
+
+    const cellButtonGroup = $('.cell-button-group');
+
+    //update button on change slide
+    $('.main-carousel').on('select.flickity', function () {
+        const cellButtons = $(this).parent('.section-carousel').next().find('.button');
+        const flkty = $(this).data('flickity');
+        const caption = $(this).parent('.section-carousel').find('.caption');
+        caption.text(flkty.selectedElement.alt);
+
+        cellButtons.filter('.is-selected')
+            .removeClass('is-selected');
+        cellButtons.eq(flkty.selectedIndex)
+            .addClass('is-selected');
+
+    });
+
+    // select cell on button click
+    cellButtonGroup.on('click', '.button', function () {
+        const carousel = $(this).parent('.section-carousel-buttons').prev().find('.main-carousel');
+        var index = $(this).index();
+        carousel.flickity('select', index);
+    });
+    // previous
+    $('.carousel-button-prev').on('click', function () {
+        $(this).parents('.section-carousel-buttons').prev().find('.main-carousel').flickity('previous');
+    });
+    // next
+    $('.carousel-button-next').on('click', function () {
+        $(this).parents('.section-carousel-buttons').prev().find('.main-carousel').flickity('next');
+    });
+
+    /*Menu*/
+    menuButton.click(function () {
+        mobileMenu.toggleClass('hide-menu');
+        mobileMenu.toggleClass('menu-closed');
+        mainContent.toggleClass('desaturate-blur');
+        $('.header').find('path').toggleClass('path-fill-white');
+    });
+
+    /*Welcome Animation*/
+    var welcomeTl = new TimelineMax();
+    welcomeTl.addLabel("start", "0")
+        .from('.container', 0.6, {
+            y: 30,
+            opacity: 0
+        }, "start+=0.5")
+        .from('.intro', 0.6, {
+            y: 60
+        }, "start+=0.3");
+
+    /*Video Controls*/
+
+    $('.video-container').click(function () {
+        const video = $(this).find('video').get(0);
+        if (video.paused) {
+            $(this).children('.video-button-title').fadeToggle(200);
+            video.play();
+        } else {
+            $(this).children('.video-button-title').fadeToggle(200);
+            video.pause();
+        }
+    });
+
+    /*svg morph*/
+
+});
