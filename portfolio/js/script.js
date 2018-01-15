22db15348adf8f7ad4508529235f67f8ebae1aef
Vitsky <issac@showbox.com>
2018-01-15 14:44:33 +0200

    worked on  animation

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -70,9 +70,99 @@ $('.video-section').click(function () {
     }
 });
 /*Welcome animation*/
-const welcomeLinesTl = new TimelineMax();
-welcomeLinesTl.add("start", 0.5).staggerFrom('.welcome-line', 0.5, {
-    y: 50,
-    opacity: 0,
-    ease: Power3.easeOut
-}, 0.1);
+if (!sessionStorage.getItem('animationPlayed')) {
+    $('body').addClass('no-scroll');
+    sessionStorage.setItem('animationPlayed', 'true');
+} else {
+    scrollLinesTl.progress(1);
+}
+const removeNoScroll = function () {
+    $('body').removeClass('no-scroll');
+
+}
+const welcomeLinesTl = new TimelineMax({
+    onComplete: removeNoScroll
+});
+
+welcomeLinesTl.add("start", 0.1)
+    .staggerFrom('.welcome-line', 1.6, {
+        opacity: 0,
+        ease: Sine.easeInOut
+    }, 0.1, "start")
+    .from('.down-arrow', 1, {
+        y: -100,
+        ease: Circ.easeOut
+    }, "start+=1");
+
+const scrollLinesTl = new TimelineMax({
+    paused: true
+});
+
+scrollLinesTl.add("start", 0)
+    .to('.welcome-line:nth-child(1)', 1, {
+        y: -300,
+        opacity: 0
+    }, "start")
+    .to('.welcome-line:nth-child(2)', 1, {
+        y: -220,
+        opacity: 0
+    }, "start")
+    .to('.welcome-line:nth-child(3)', 1, {
+        y: -140,
+        opacity: 0
+    }, "start")
+    .to('.welcome-line:nth-child(4)', 1, {
+        y: -60,
+        opacity: 0
+    }, "start");
+
+/*Play animation with scroll*/
+$(window).scroll(function (e) {
+    let scrollTop = $(window).scrollTop();
+    //let docHeight = $(document).height();
+    let winHeight = $('.welcome-container').height();
+    let scrollPercent = (scrollTop) / (winHeight);
+    let scrollPercentRounded = Math.round(scrollPercent * 100) / 100;
+
+    $('#scrollPercentLabel>span').html(scrollPercentRounded);
+
+    scrollLinesTl.progress(scrollPercent).pause();
+});
+
+/*
+Smooth Scrolling*/
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 800, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
