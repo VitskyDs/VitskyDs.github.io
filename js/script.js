1f28edc01219c0df063370834833705d45fb16fb
Isaac Sheptovitsky <isaacsheptovitsky@Isaacs-MBP.Dlink>
2019-02-02 13:46:23 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,62 +1,3 @@
-const mobileMenu = $('.mobile-menu'),
-    cellButtonGroup = $('.cell-button-group');
-
-const removeNoScroll = function () {
-    $('body').removeClass('no-scroll');
-}
-
-/*Initialize Flickity Carousels*/
-$('.main-carousel').flickity({
-    cellAlign: 'center',
-    contain: true,
-    imagesLoaded: true,
-    setGallerySize: true,
-    prevNextButtons: false,
-    pageDots: false,
-    cellSelector: '.carousel-cell',
-    wrapAround: true
-});
-
-//update button on change slide
-$('.main-carousel').on('select.flickity', function () {
-    const cellButtons = $(this).parent('.section-carousel').next().find('.button'),
-        flkty = $(this).data('flickity'),
-        caption = $(this).parent('.section-carousel').find('.caption'),
-        selectedImg = $(this).find('.is-selected.carousel-cell').find('img');
-
-    caption.text(selectedImg.attr('alt'));
-    cellButtons.filter('.is-selected').removeClass('is-selected');
-    cellButtons.eq(flkty.selectedIndex).addClass('is-selected');
-});
-
-// select cell on button click
-cellButtonGroup.on('click touchstart', '.button', function () {
-    const carousel = $(this).parent('.section-carousel-buttons').prev().find('.main-carousel');
-    var index = $(this).index();
-    carousel.flickity('select', index);
-});
-// previous
-$('.carousel-button-prev').on('click touchstart', function () {
-    $(this).parents('.section-carousel-buttons').prev().find('.main-carousel').flickity('previous');
-});
-// next
-$('.carousel-button-next').on('click touchstart', function () {
-    $(this).parents('.section-carousel-buttons').prev().find('.main-carousel').flickity('next');
-});
-
-/*Menu*/
-
-const toggleMenu = function () {
-        mobileMenu.toggleClass('menu-closed');
-        $('.menu-button').toggleClass('menu-button-close');
-        $('header').find('path').toggleClass('path-fill-white');
-        $('body').toggleClass('position-fixed');
-    }
-    //toggles menu
-$('body').on('click touchstart', '.menu-button, .menu-link', function () {
-    toggleMenu();
-});
-
 /*Welcome animation*/
 
 const scrollLinesTl = new TimelineMax({
@@ -87,51 +28,13 @@ $(window).scroll(function (e) {
         winHeight = $('.welcome-container').height(),
         scrollPercent = (scrollTop) / (winHeight + 1200),
         scrollPercentRounded = Math.round(scrollPercent * 100) / 100;
-
     $('#scrollPercentLabel>span').html(scrollPercentRounded);
 
     scrollLinesTl.progress(scrollPercent).pause();
 });
 
-/*showreel animation*/
 
-const showreelTl = new TimelineMax({
-    repeat: -1,
-    repeatDelay: 2
-});
-
-showreelTl.add("middle", 0.75).add("end", 2.5).fromTo('.particle', 0.30, {
-    y: -50,
-    x: 250,
-    ease: Expo.easeIn
-}, {
-    y: -50,
-    x: -250,
-}).fromTo('.particle', 0.30, {
-    y: 50,
-    x: -250,
-    ease: Expo.easeIn
-}, {
-    y: 50,
-    x: 250
-}).fromTo('.particle', 0.20, {
-    y: 0,
-    x: 200
-}, {
-    y: 0,
-    x: 95,
-    ease: Expo.easeOut
-}, "middle").fromTo('.particle', 0.75, {
-    y: 0,
-    rotation: 0
-}, {
-    y: 150,
-    rotation: 220,
-    ease: Power4.easeIn
-}, "end");
-
-/*
-Smooth Scrolling*/
+/*Smooth Scrolling*/
 // Select all links with hashes
 $('a[href*="#"]')
     // Remove links that don't actually link to anything
