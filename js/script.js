dd637588a7d5cbbf8a4f50429daff5785fdae1ab
Isaac Sheptovitsky <isaacsheptovitsky@Isaacs-MBP.Dlink>
2019-02-04 10:10:48 +0200

    project styling

--- js/script.js
+++ js/script.js
@@ -1,39 +1,3 @@
-/*Welcome animation*/
-
-const scrollLinesTl = new TimelineMax({
-    paused: true
-});
-
-scrollLinesTl.add("start", 0)
-    .to('.welcome-line:nth-child(1)', 1, {
-        y: -300,
-        opacity: 0
-    }, "start")
-    .to('.welcome-line:nth-child(2)', 1, {
-        y: -220,
-        opacity: 0
-    }, "start")
-    .to('.welcome-line:nth-child(3)', 1, {
-        y: -140,
-        opacity: 0
-    }, "start")
-    .to('.welcome-line', 1, {
-        opacity: 1,
-        y: 0
-    });
-
-/*Play animation with scroll*/
-$(window).scroll(function (e) {
-    let scrollTop = $(window).scrollTop(),
-        winHeight = $('.welcome-container').height(),
-        scrollPercent = (scrollTop) / (winHeight + 1200),
-        scrollPercentRounded = Math.round(scrollPercent * 100) / 100;
-
-    $('#scrollPercentLabel>span').html(scrollPercentRounded);
-
-    scrollLinesTl.progress(scrollPercent).pause();
-});
-
 /*
 Smooth Scrolling*/
 // Select all links with hashes
